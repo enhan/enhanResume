e937a77c7fb9ca3b71cc0733b9edb438abd924b5
Emmanuel Nhan <nhanmanu@gmail.com>
2014-07-07 21:50:03 +0200

    initial app (nothing dynamic)

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name enhanResumeApp
+ * @description
+ * # enhanResumeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('enhanResumeApp', [
+    'ngAnimate'
+  ]);
